7d023827ed8a5dde4cf04a929d396121adb090b3
Sameerchoudhary8093 <Sameerchoudhary8093@gmail.com>
2026-01-16 17:41:04 +0530

    refactor: use Vercel api folder pattern

---
+++ api/index.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+
+export default async function handler(req: any, res: any) {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({ origin: true, credentials: true });
+    await app.init();
+    const instance = app.getHttpAdapter().getInstance();
+    return instance(req, res);
+}
